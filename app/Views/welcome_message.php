c118b61b268a2251172489c0db9b728a98f844ca
kenjisupremacy <79846278+kenjisupremacy@users.noreply.github.com>
2021-03-16 15:45:34 -0400

    bootstrap

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="<?php echo base_url('styles.css')?>" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"></script>
+		<title>Materiel.net</title>
+    </head>
+    <body>
+	<div class="main">
+        <div class="container-fluid">
+			<div class="row">
+				<div id="block1" class="col-12" style="text-align:center">
+					<img src="images/banniere.jpg" class="img-responsive">
+				</div>
+			</div>
+				<div id="block2" class="row" style="">
+					<div class="col-xl-6 col-md-12" style="text-align:center">
+						<img src="images/pcgamer.jpg" class="image-1">
+					</div>
+					<div class="col-xl-6 col-md-12" style="text-align:center" >
+						<p class="param-1" style="font-weight: bold;">Avec le ROG Zephyrus Duo GX550LWS-54T, Asus nous propose un condensé de toutes ses dernières innovations techniques comme son système de refroidissement novateur AAS ou son double écran pour un prolongement de votre espace de jeu. C'est aussi l'occasion pour Asus d'intégrer les toutes dernières nouveautés en terme de composant, comme sa dalle 300 Hz, sa carte graphique NVIDIA RTX 2070 Super Max-Q ou son processeur 8 coeurs Comet Lake. En bref, un véritable condensé de technologie et de puissance dans un portable que l'on peut qualifier d'exceptionnel !</p>
+					</div>
+				</div>	
+
+				<div id="block3" class="row" style="">
+					<div class="col-xl-6 order-xl-2 col-md-12" style="text-align:center" >
+						<img src="images/pcgamer1.jpg" class="image-2">
+					</div>
+					<div class="col-xl-6 col-md-12" style="text-align:center" >
+						<p class="param-2" style="font-weight: bold;">Avec le ROG Zephyrus S15 GX532LXS-13T, Asus nous propose une machine puissante taillée pour une mobilité extrême. Design audacieux et composants ultra haut de gamme se donnent donc rendez vous dans ce dernier né de la gamme ROG Zephyrus pour vous donner accès à une expérience de jeu unique dans un portable que l'on peut véritablement qualifier d'exceptionnel !</p>
+					</div>
+				</div>	
+
+				<div id="block4" class="row" style="">
+					<div class="col-xl-3 col-md-6" style="text-align:center">
+						<img src="images/dollar.jpg" width="100%" class="rounded-circle float-right">
+					</div>
+					<div class="col-xl-3 col-md-6" style="text-align:center" >
+						<img src="images/dollar.jpg" width="100%" class="rounded-circle float-right">
+					</div>
+					<div class="col-xl-3 col-md-6" style="text-align:center" >
+						<img src="images/dollar.jpg" width="100%" class="rounded-circle float-right">
+					</div>
+					<div class="col-xl-3 col-md-6" style="text-align:center" >
+						<img src="images/dollar.jpg" width="100%" class="rounded-circle float-right">
+					</div>
+				</div>	
+        </div>
+	</div>
+    </body>
+
+
+</html>
